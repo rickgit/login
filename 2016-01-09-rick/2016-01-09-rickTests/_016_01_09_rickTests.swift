763f79c53074fbf756103531cce23eec82b99310
Rick Smith <wangash@qq.com>
2016-01-10 00:03:43 +0800

    提交代码，登录的布局还是有问题 科科

---
+++ 2016-01-09-rick/2016-01-09-rickTests/_016_01_09_rickTests.swift
@@ -0,0 +1,36 @@
+//
+//  _016_01_09_rickTests.swift
+//  2016-01-09-rickTests
+//
+//  Created by Anshu Wang on 16/1/9.
+//  Copyright (c) 2016年 rick. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class _016_01_09_rickTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
